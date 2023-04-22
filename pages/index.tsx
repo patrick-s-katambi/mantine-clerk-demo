e9c4f6d6817cf0df6b7d2c7c28c6d598b618e5a8
patrick-s-katambi <patrick.simon.kata,bi@gmail.com>
2023-04-22 22:04:34 +0300

    navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Navbar } from '~/components/Navbar/Navbar';
 
 export default function HomePage() {
   return (
     <>
+      <Navbar />
       <Welcome />
       <ColorSchemeToggle />
     </>
